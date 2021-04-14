fbfd8163958fd135d2832c9ade4f101a09fe400b
Jeffrey Arnell <arnellj@byu.edu>
2021-04-14 13:39:48 -0600

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -30,7 +30,9 @@ import { Router, RouterModule } from '@angular/router';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/linear-kinematics/linear-kinematics.component.ts
+++ src/app/linear-kinematics/linear-kinematics.component.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./linear-kinematics.component.css']
 })
 export class LinearKinematicsComponent implements OnInit {
-
+  public iVelocity: any;
+  public fVelocity: any;
+  public iAcceleration: any;
+  public fAcceleration: any;
+  public time: any;
+  public fDistance: any;
+  public iDistance: any;
+  public constantAccel: string = "";
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  find_iVelocity(){
+    if (this.fVelocity && this.fAcceleration && this.time){
+
+    }
+    
+    if (this.fVelocity && this.fAcceleration && this.iDistance && this.fDistance){
+
+    }
+  }
+
+  find_fVelocity(){
+
+  }
+
+  find_iAcceleration(){
+
+  }
+
+  find_fAcceleration(){
+
+  }
+
+  find_time(){
+
+  }
+
+  find_distance(){
+
+  }
+
+  find_displacement(){
+
+  }
 }
